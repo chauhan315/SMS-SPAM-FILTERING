a863b91fd87c37a6cf58185cfb2761ef843ba204
Ankur Singh Chauhan <ankur3382@outlook.com>
2025-04-14 20:24:21 +0530

    update in load model to check for cpu or gpu

--- load_model.py
+++ load_model.py
@@ -6,4 +6,13 @@ MODEL_PATH = "Model"
 def load_model():
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+   	
+    if next(model.parameters()).device.type == "meta":
+        model = model.to_empty(device)
+    else:
+        model = model.to(device)
+    
+
     return tokenizer, model
